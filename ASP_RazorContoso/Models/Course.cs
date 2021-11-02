c860f5f2c1f3ed1fb66b325beb9313501c37f1e0
Tyronne282 <bradburntyronne@gmail.com>
2021-11-02 15:13:07 +0000

    Finished Video tutorial

---
+++ ASP_RazorContoso/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP_RazorContoso.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+
+        public string CorseCose { get; set; }
+
+        public string Title { get; set; }
+
+        public int Credits { get; set; }
+
+        // navigation properties
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
